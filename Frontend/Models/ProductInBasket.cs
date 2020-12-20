5838a576cb46fa7f200a67f76af33c55871c036a
Nor Shiervani <norsh@LAPTOP-8TKU7Q60>
2020-12-20 21:15:34 +0100

    Add products to basket (saved in local storage) and small updates to UI.

---
+++ Frontend/Models/ProductInBasket.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Frontend.Models
+{
+    public class ProductInBasket
+    {
+        public Product Product { get; set; }
+        public int Amount { get; set; }
+    }
+}
