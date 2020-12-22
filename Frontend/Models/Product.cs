0c8feb171c9036f163d20623f9a9fe8860b295b4
irvin <irvin_b23@hotmail.com>
2020-12-22 10:03:44 +0100

    Update from test review

--- Frontend/Models/Product.cs
+++ Frontend/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,14 @@ namespace Frontend.Models
     public class Product
     {
         public int Id { get; set; }
+        [Required]
+        [Range(1, 500, ErrorMessage = "Stock must be between 1 and 500 .")]
         public int Stock { get; set; }
+        [Required]
+        [StringLength(16, ErrorMessage = "Name too long (16 character limit).")]
         public string Name { get; set; }
+        [Required]
+        [StringLength(250, ErrorMessage = "Description too long (250 character limit).")]
         public string Description { get; set; }
         public bool IsAvailable { get; set; }
         public int ProductCategoryId { get; set; }
