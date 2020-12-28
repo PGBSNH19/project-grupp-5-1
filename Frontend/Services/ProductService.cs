b75ae3254f64470da1aa31ba05e4761caa9f53aa
irvin <irvin_b23@hotmail.com>
2020-12-28 16:13:06 +0100

    Admin update product details[G5-10]

--- Frontend/Services/ProductService.cs
+++ Frontend/Services/ProductService.cs
@@ -34,5 +34,12 @@ namespace Frontend.Services
             return await httpClient.PostJsonAsync<Product>(_configuration["ApiHostUrl"] + "api/v1.0/products", product);
         }
 
+        public async Task<Product> Update(Product product)
+        {
+            //product.Id = null;
+            return await httpClient.PutJsonAsync<Product>(_configuration["ApiHostUrl"] + $"api/v1.0/products/{product.Id}", product);
+        }
+
+
     }
 }
